679a58266040201420e1c77d5cdc9ae40bea8c20
Nobeji <alphaxadnobeji@gmail.com>
2026-07-13 07:35:53 -0700

    fix: OptionalJWTAuthentication - don't fail on expired tokens for AllowAny endpoints

--- config/settings.py
+++ config/settings.py
@@ -102,7 +102,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'config.authentication.OptionalJWTAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.AllowAny',
